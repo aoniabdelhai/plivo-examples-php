f30fd489a975474acaaa2b3a2efc6e246ecceb33
tsudot <kunalkerkar@gmail.com>
2012-03-21 19:37:52 +0530

    More examples with output

--- plivo_xml.php
+++ plivo_xml.php
@@ -5,29 +5,31 @@
     $auth_token = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
 
     $body = 'Hi, Calling from Plivo';
+    $url = 'http://examples.com/playTrumpet.mp3'
     $attributes = array (
-        'loop' => 5,
+        'loop' => 2,
     );
 
     $r = new Response();
 
     // Add speak element
     $r->addSpeak($body, $attributes);
-    echo $r->toXML();
 
     // Add play element
     $r->addPlay($body, $attribute);
-    echo $r->toXML();
 
     // Add wait element
     $wait_attribute = array(
-        'length' => 2,
+        'length' => 3,
     );
     $r->addWait($wait_attribute);
 
-
-
-
+    //  Output:
+    //  <Response>
+    //  <Speak loop="2">Calling from Plivo</Speak>
+    //  <Play loop="2">http://examples.com/playTrumpet.mp3</Play>
+    //  <Wait length="3" />
+    //  </Response>
 
 
 
