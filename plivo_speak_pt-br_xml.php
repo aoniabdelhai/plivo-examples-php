a9ea97ce758747b633dc01a6a58c7296dd3a79ab
keshavagrawal89 <keshav_agrawal89@hotmail.com>
2012-09-25 03:51:50 +0530

    Update plivo_speak_pt-br_xml.php

--- plivo_speak_pt-br_xml.php
+++ plivo_speak_pt-br_xml.php
@@ -1,6 +1,6 @@
 <?php
     require_once 'plivo.php';
-
+    //This encoding technique is for lower versions of php. Tested on php 5.3.10
     $convmap = array(0x80, 0xffff, 0, 0xffff);
     $body = mb_encode_numericentity("A rainha do aché comeu 1 maçã, 2 mamões e outras frutas às 15hs do dia 23/5/2012. Parabéns e abraços do vovô e da vovó.", $convmap, 'UTF-8'); 
     $url = 'http://examples.com/playTrumpet.mp3';
