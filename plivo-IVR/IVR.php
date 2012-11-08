57976c31223e2329e6336e319c8ce7a242820a2c
keshavagrawal89 <keshav_agrawal89@hotmail.com>
2012-11-09 03:41:00 +0530

    added a new example for IVR

---
+++ plivo-IVR/IVR.php
@@ -0,0 +1,20 @@
+<?php
+    require_once 'plivo.php';
+
+    $body = 'Hi, Calling from Plivo. Please enter 1 for music and 2 for Speak.';
+   // $url = 'http://examples.com/playTrumpet.mp3';
+    $attributes = array (
+        'loop' => 2,
+    );
+    $getdigitattributes = array (
+	'action'=> 'http://someserver.com/gather',
+    );
+
+    $r = new Response();
+    $g = addGetDigits($getdigitattributes);
+    $g->addSpeak($body,$attributes);
+    $r->add($g);
+    $r->addSpeak("Input not recieved",array('language' => 'en-US', 'voice' => 'WOMAN'));
+    echo($response->toXML());
+
+?>
