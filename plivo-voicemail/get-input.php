9e17b89d7f8f4a659e860f744676764b1b5500e7
Rakesh Kadarla <rakesh@plivo.com>
2013-02-24 22:53:04 -0800

    Update plivo-voicemail/get-input.php

--- plivo-voicemail/get-input.php
+++ plivo-voicemail/get-input.php
@@ -3,13 +3,13 @@
 	
 	$response = new Response();
 	$response->addSpeak('Record your message at the tone, after you are done, press pound');
-	$response->addRecord(array('action' => 'http://' . $_SERVER["SERVER_NAME"] . ':' . /*$_SERVER["SERVER_PORT"] .*/ '/pep/plivo-voicemail/confirm-input.php',
+	$response->addRecord(array('action' => 'http://' . $_SERVER["SERVER_NAME"] . /*':' . $_SERVER["SERVER_PORT"] .*/ '/pep/plivo-voicemail/confirm-input.php',
 						   'method' => 'GET',
 						   'maxLength' => '60',
 						   'finishOnKey' => '#',
 						   'playBeep' => 'true'));
 	$response->addSpeak('Recording not received');
-	$response->addRedirect('http://' . $_SERVER["SERVER_NAME"] . ':' . /*$_SERVER["SERVER_PORT"] .*/ '/pep/plivo-voicemail/get-input.php', array('method' => 'GET'));
+	$response->addRedirect('http://' . $_SERVER["SERVER_NAME"] . /*':' . $_SERVER["SERVER_PORT"] .*/ '/pep/plivo-voicemail/get-input.php', array('method' => 'GET'));
 	header('content-type: text/xml');
 	echo($response->toXML());
 ?>
